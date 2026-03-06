098873055ac3c0538abb8d0d5f39b846251284b4
fed663 <fedya-ismailov-2002@mail.ru>
2026-03-06 22:11:12 +0300

    first vision

---
+++ deque/deque.go
@@ -0,0 +1,43 @@
+package deque
+
+import (
+	"cmp"
+)
+
+type Deque[T cmp.Ordered] struct {
+	data []T
+}
+
+// добавить в начало
+func (d *Deque[T]) PushFront(val T) {
+	d.data = append([]T{val}, d.data...)
+}
+
+// добавить в конец
+func (d *Deque[T]) PushBack(val T) {
+	d.data = append(d.data, val)
+}
+
+// взять из начала
+func (d *Deque[T]) PopFront() (T, bool) {
+	var nol T
+	if len(d.data) == 0 {
+		return nol, false
+	}
+
+	val := d.data[0]
+	d.data = d.data[1:]
+	return val, true
+}
+
+// взять из конца
+func (d *Deque[T]) PopBack() (T, bool) {
+	var nol T
+	if len(d.data) == 0 {
+		return nol, false
+	}
+
+	val := d.data[len(d.data)-1]
+	d.data = d.data[:len(d.data)-1]
+	return val, true
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fed663/myTools
+
+go 1.25.5
