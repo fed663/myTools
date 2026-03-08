69ad67389aaf92b9137bb924af2fb8316346e66d
fed663 <fedya-ismailov-2002@mail.ru>
2026-03-08 21:47:49 +0300

    add ring buffer alg

--- deque/deque.go
+++ deque/deque.go
@@ -1,39 +1,113 @@
 package deque
 
+import "fmt"
+
 type Deque[T any] struct {
 	data []T
+	head int
+	tail int
+	size int
+}
+
+// конструктор 2 в 1
+func NewDeque[T any](size1 ...int) *Deque[T] {
+	if len(size1) > 1 {
+		panic("NewDeque takes up to one argument.")
+	}
+	size0 := 8
+	if len(size1) > 0 && size1[0] > 0 {
+		size0 = size1[0]
+	}
+	return &Deque[T]{data: make([]T, size0)}
+}
+
+// блок инициализации внутреннего среза
+func (d *Deque[T]) initBlock() {
+	if len(d.data) == 0 {
+		d.data = make([]T, 8)
+	}
+}
+
+// проверка пустой структуры
+func (d *Deque[T]) IsEmpty() bool {
+	return d.size == 0
+}
+
+// функция увелечения вложенного среза
+func (d *Deque[T]) grow() {
+	if d.size < len(d.data) {
+		return
+	}
+	newData := make([]T, len(d.data)*2)
+	for i := 0; i < d.size; i++ {
+		newData[i] = d.data[(d.head+i)%len(d.data)]
+	}
+
+	d.data = newData
+	d.head = 0
+	d.tail = d.size
+}
+
+func (d *Deque[T]) Len() int {
+	return d.size
+}
+
+func (d *Deque[T]) String() string {
+	res := make([]T, d.size)
+
+	for i := 0; i < d.size; i++ {
+		res[i] = d.data[(d.head+i)%len(d.data)]
+	}
+
+	return fmt.Sprint(res)
 }
 
 // добавить в начало
 func (d *Deque[T]) PushFront(val T) {
-	d.data = append([]T{val}, d.data...)
+	d.initBlock()
+	d.grow()
+
+	d.head = (d.head - 1 + len(d.data)) % len(d.data)
+	d.data[d.head] = val
+	d.size++
 }
 
 // добавить в конец
 func (d *Deque[T]) PushBack(val T) {
-	d.data = append(d.data, val)
+	d.initBlock()
+	d.grow()
+
+	d.data[d.tail] = val
+	d.tail = (d.tail + 1) % len(d.data)
+	d.size++
 }
 
 // взять из начала
 func (d *Deque[T]) PopFront() (T, bool) {
-	var nol T
-	if len(d.data) == 0 {
-		return nol, false
+	var nul T
+	if d.size == 0 {
+		return nul, false
 	}
 
-	val := d.data[0]
-	d.data = d.data[1:]
+	val := d.data[d.head]
+	d.data[d.head] = nul
+	d.head = (d.head + 1) % len(d.data)
+	d.size--
+
 	return val, true
 }
 
 // взять из конца
 func (d *Deque[T]) PopBack() (T, bool) {
-	var nol T
-	if len(d.data) == 0 {
-		return nol, false
+	var nul T
+	if d.size == 0 {
+		return nul, false
 	}
 
-	val := d.data[len(d.data)-1]
-	d.data = d.data[:len(d.data)-1]
+	d.tail = (d.tail - 1 + len(d.data)) % len(d.data)
+	val := d.data[d.tail]
+	d.data[d.tail] = nul
+	d.size--
+
 	return val, true
 }
